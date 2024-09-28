4c9336145cb4e8bd32e170b55dded0a739f84ea4
JakimPL <jakimpl@gmail.com>
2024-09-28 08:40:31 +0200

    Added: duty cycle

--- src/osc/square.asm
+++ src/osc/square.asm
@@ -1,24 +1,28 @@
+    %define OSC_SQUARE_PARAMETERS 1
+    %define OSC_SQUARE_DUTY_CYCLE 0
+
     section .text
 square:
     movzx eax, byte [current_channel]
     mov eax, [oscillator_timer + 4 * eax]
     mov ecx, [dividend]
-    shr ecx, 1
+.duty_cycle:
+    shr ecx, 8
+    imul ecx, 0x80
     mov ebx, eax
     cmp eax, ecx
+
+    mov ax, BASE_VOLUME
+    mov bx, [volume]
 
     jl .low_value
     jmp .high_value
 
 .low_value:
-    mov ax, BASE_VOLUME
-    mov bx, [volume]
     add ax, bx
     jmp .done
 
 .high_value:
-    mov ax, BASE_VOLUME
-    mov bx, [volume]
     sub ax, bx
 
 .done:
