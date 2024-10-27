c371337d344944123adc4b3438274c1170d224c8
JakimPL <jakimpl@gmail.com>
2024-10-27 13:25:13 +0100

    Added: naive IDFT

--- src/utils/fft.asm
+++ src/utils/fft.asm
@@ -1,6 +1,9 @@
     SEGMENT_CODE
 generate_fft_twiddles:
+.load_fft_size:
     mov cx, FFT_SIZE
+    mov [fft_size], cx
+.load_angle_constant:
     mov di, fft_twiddles
     fld dword [fft_angle_constant]
     fldz
@@ -23,6 +26,7 @@ generate_fft_twiddles:
     ret
 
 fft:
+    mov cl, [fft_inverse]
     fninit
     pusha
     xor edi, edi
@@ -31,7 +35,7 @@ fft:
     fldz
     fldz
 
-    mov ecx, FFT_SIZE
+    movzx ecx, word [fft_size]
     xor esi, esi
     mov ebx, edi
 
@@ -39,6 +43,11 @@ fft:
     xor edx, edx
     mov eax, edi
     mul esi
+    cmp byte [fft_inverse], 1
+    jne .get_twiddle_index
+.inverse:
+    neg eax
+.get_twiddle_index:
     div ecx
     mov ebx, edx
 
@@ -70,21 +79,35 @@ fft:
     fstp st0
 
 ; Increment
-    inc esi
-    cmp esi, FFT_SIZE
+    inc si
+    cmp si, [fft_size]
     jnz .inner_loop
 
 ; Store output
+    cmp byte [fft_inverse], 1
+    jne .store_output
+.normalize:
+    fild word [fft_size]
+    fdiv st1, st0
+    fdiv st2, st0
+    fstp st0
+.store_output:
     fstp dword [fft_output + 8 * edi + 4]
     fstp dword [fft_output + 8 * edi]
 
-    inc edi
-    cmp edi, FFT_SIZE
+    inc di
+    cmp di, [fft_size]
     jnz .outer_loop
     popa
+    ret
+
+fft_divide:
+    mov cl, [fft_inverse]
     ret
 
     SEGMENT_BSS
     fft_twiddles resq FFT_SIZE
     fft_input resq FFT_SIZE
     fft_output resq FFT_SIZE
+    fft_size resw 1
+    fft_inverse resb 1
