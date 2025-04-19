acb78769a46da36df567f0ee22f179c6f4eeb5f4
JakimPL <jakimpl@gmail.com>
2025-04-19 11:06:19 +0200

    Added: interpolation for 8 bit

--- core/utils/table.asm
+++ core/utils/table.asm
@@ -35,25 +35,18 @@ load_table_16bit_item:
 ; ECX - divisor
 ; SI  - table address
 ; AX  - input timer/output
-    mul ebx
-    div ecx
-    shr ecx, 16
-    mov [value], ecx
-    shr edx, 16
-    mov [angle], edx
-    xor edx, edx
-    dec bx
+    call load_table_item
+    jc .no_interpolation
+.interpolation:
+    call sample_interpolation_set
     mov cx, [esi + 2 * eax]
-    cmp ax, bx
-    jne .load_items
-.modulo:
-    sub ax, bx
-.load_items:
+    call sample_interpolation_modulo
     mov bx, [esi + 2 * eax + 2]
-    lea esi, [angle]
-    lea edi, [value]
-    call interpolate
-    movzx eax, ax
+    call sample_interpolation_apply
+    jmp .done
+.no_interpolation:
+    mov ax, [esi + 2 * eax]
+.done:
     ret
 
 load_table_8bit_item:
@@ -61,7 +54,45 @@ load_table_8bit_item:
 ; ECX - divisor
 ; SI  - table address
 ; AX  - input timer/output
+    call load_table_item
+    jc .no_interpolation
+.interpolation:
+    call sample_interpolation_set
+    movzx cx, byte [esi + eax]
+    call sample_interpolation_modulo
+    movzx bx, byte [esi + eax + 1]
+    call sample_interpolation_apply
+    jmp .done
+.no_interpolation:
+    mov al, [esi + eax]
+.done:
+    ret
+
+load_table_item:
     mul ebx
     div ecx
-    mov al, [esi + eax]
+    ret
+
+sample_interpolation_set:
+    shr ecx, 16
+    mov [value], ecx
+    shr edx, 16
+    mov [angle], edx
+    xor edx, edx
+    dec bx
+    ret
+
+sample_interpolation_modulo:
+    cmp ax, bx
+    je .modulo
+    ret
+.modulo:
+    sub ax, bx
+    ret
+
+sample_interpolation_apply:
+    lea esi, [angle]
+    lea edi, [value]
+    call interpolate
+    movzx eax, ax
     ret
