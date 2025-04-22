ecda858050ac2ee2ef9e908afd9de89a128f358d
JakimPL <jakimpl@gmail.com>
2025-04-22 16:36:01 +0200

    Changed: routing initialization strategy

--- tracker/maps/routing.hpp
+++ tracker/maps/routing.hpp
@@ -11,13 +11,16 @@
 
 typedef std::tuple<std::vector<size_t>, std::vector<std::string>, std::vector<uint16_t>, std::vector<TargetVariableType>> RoutingTuple;
 
+struct RoutingItem {
+    std::string label;
+    uint16_t offset;
+    TargetVariableType type;
+    int constraint = -1;
+    int default_shift = 0;
+};
+
 struct RoutingItems {
-    RoutingItems(
-        std::vector<std::string> labels = {},
-        std::vector<uint16_t> offsets = {},
-        std::vector<TargetVariableType> types = {},
-        std::vector<int> constraints = {}
-    );
+    RoutingItems(std::vector<RoutingItem> items);
 
     std::vector<std::string> labels;
     std::vector<uint16_t> offsets;
@@ -32,109 +35,44 @@ const std::map<Target, RoutingItems> routing_variables = {
     {
         Target::ENVELOPE,
         RoutingItems({
-            {
-                "Base volume",
-                "Sustain level",
-                "Attack",
-                "Decay",
-                "Hold",
-                "Release",
-            },
-            {
-                ENVELOPE_BASE_VOLUME,
-                ENVELOPE_SUSTAIN_LEVEL,
-                ENVELOPE_ATTACK,
-                ENVELOPE_DECAY,
-                ENVELOPE_HOLD,
-                ENVELOPE_RELEASE,
-            },
-            {
-                TargetVariableType::Int16,
-                TargetVariableType::Int16,
-                TargetVariableType::Int16,
-                TargetVariableType::Int16,
-                TargetVariableType::Int16,
-                TargetVariableType::Int16,
-            },
-            {
-                -1,
-                -1,
-                -1,
-                -1,
-                -1,
-                -1,
-            },
+            {"Base volume", ENVELOPE_BASE_VOLUME, TargetVariableType::Int16},
+            {"Sustain level", ENVELOPE_SUSTAIN_LEVEL, TargetVariableType::Int16},
+            {"Attack", ENVELOPE_ATTACK, TargetVariableType::Int16},
+            {"Decay", ENVELOPE_DECAY, TargetVariableType::Int16},
+            {"Hold", ENVELOPE_HOLD, TargetVariableType::Int16},
+            {"Release", ENVELOPE_RELEASE, TargetVariableType::Int16},
         }),
     },
     {
         Target::SEQUENCE,
-        RoutingItems(),
+        RoutingItems({}),
     },
     {
         Target::ORDER,
-        RoutingItems(),
+        RoutingItems({}),
     },
     {
         Target::OSCILLATOR,
         RoutingItems({
-            {
-                "Duty cycle",
-            },
-            {
-                OSCILLATOR_SQUARE_DUTY_CYCLE,
-            },
-            {
-                TargetVariableType::Int8,
-            },
-            {
-                GENERATOR_SQUARE,
-            },
+            {"Duty cycle", OSCILLATOR_SQUARE_DUTY_CYCLE, TargetVariableType::Int8, GENERATOR_SQUARE},
         }),
     },
     {
         Target::WAVETABLE,
-        RoutingItems(),
+        RoutingItems({}),
     },
     {
         Target::DSP,
         RoutingItems({
-            {
-                "Gain",
-                "Level",
-                "Cutoff frequency",
-            },
-            {
-                DSP_GAINER_VOLUME,
-                DSP_DISTORTION_LEVEL,
-                DSP_FILTER_FREQUENCY,
-            },
-            {
-                TargetVariableType::Int16,
-                TargetVariableType::Int16,
-                TargetVariableType::Int16,
-            },
-            {
-                EFFECT_GAINER,
-                EFFECT_DISTORTION,
-                EFFECT_FILTER,
-            },
+            {"Gain", DSP_GAINER_VOLUME, TargetVariableType::Int16, EFFECT_GAINER},
+            {"Level", DSP_DISTORTION_LEVEL, TargetVariableType::Int16, EFFECT_DISTORTION},
+            {"Cutoff frequency", DSP_FILTER_FREQUENCY, TargetVariableType::Int16, EFFECT_FILTER},
         }),
     },
     {
         Target::CHANNEL,
         RoutingItems({
-            {
-                "Pitch",
-            },
-            {
-                CHANNEL_PITCH,
-            },
-            {
-                TargetVariableType::Int32,
-            },
-            {
-                -1,
-            },
+            {"Pitch", CHANNEL_PITCH, TargetVariableType::Int32},
         }),
     },
 };
