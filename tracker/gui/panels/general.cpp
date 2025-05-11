08a4cb6252ef1439834490d7f9f036c9c8410abb
JakimPL <jakimpl@gmail.com>
2025-05-11 15:46:33 +0200

    Fixed: normalization of all output channels

--- tracker/gui/panels/general.cpp
+++ tracker/gui/panels/general.cpp
@@ -224,8 +224,8 @@ void GUIGeneralPanel::draw_tempo() {
 
 void GUIGeneralPanel::draw_output() {
     ImGui::Text("Output");
-    draw_float_slider("Master gainer", current_song.normalizer, {}, 0.01f, 2.0f);
     ImGui::BeginDisabled(gui.is_playing());
+    draw_float_slider("Master gainer", current_song.normalizer, {}, 0.01f, 2.0f);
     draw_float_slider("Sample rate", current_song.sample_rate, {}, GUI_MIN_SAMPLE_RATE, GUI_MAX_SAMPLE_RATE, GUIScale::Linear, "%.0f");
     draw_int_slider("Output channels", current_song.output_channels, {}, 1, MAX_OUTPUT_CHANNELS);
     ImGui::EndDisabled();
